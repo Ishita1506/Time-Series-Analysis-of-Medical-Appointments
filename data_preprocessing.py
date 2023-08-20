10b0e5fef9b943395542b05b3535fa3b6b5c5ab0
Ishita1506 <115528664+Ishita1506@users.noreply.github.com>
2023-08-20 09:47:59 +0200

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -47,4 +47,54 @@ def rename_columns():
     df.rename(columns=column_mapping, inplace=True)
     return df
 
+def drop_columns():
+    df = m1.rename_columns()
+    # Method to drop unnecessary columns from the DataFrame.
+    # Returns: DataFrame with specified columns dropped.
+    columns_to_drop = ['PatientId', 'AppointmentID', 'Neighbourhood']
+    df.drop(columns=columns_to_drop, inplace=True)
+    return df
+
+
+def create_bin():
+    ds = drop_columns()
+    #First Drop rows with Age == 0
+    ds = ds[ds['Age'] != 0]
+    # Generating labels for age intervals (e.g., '1 - 20', '21 - 40', etc.)
+    labels = ["{0} - {1}".format(i, i + 20) for i in range(1, 118, 20)]
+    # Using the pd.cut() function to categorize ages into groups(use bins = range(1, 130, 20) ,right=False and use the given labels)
+    ds['Age_group'] = pd.cut(ds['Age'], bins = range(1, 130, 20), right = False, labels = labels)
+    # Returning the modified dataset with assigned age groups
+    return ds
+
+def drop():
+    df = create_bin()
+    # Method to drop the original 'Age' column from the DataFrame.
+    # Returns: DataFrame with the 'Age' column dropped.
+    df.drop(columns=['Age'], inplace=True)
+    return df
+
+
+def convert():
+    df = drop()
+    # Method to convert 'NoShow' values into binary values (1 for 'Yes' and 0 for 'No').
+    # Returns: DataFrame with 'NoShow' column values converted to 1s and 0s.
+    df['NoShow'] = df['NoShow'].map({'Yes': 1, 'No': 0})
+    return df
+
+
+def export_the_dataset():
+    df = convert()
+    # write your code to export the cleaned dataset and set the index=false and return the same as 'df'
+    df.to_csv('patients.csv', index=False)
+    return df
+
+# TASK 6: Load the Cleaned dataset 'patients.csv' to the database provided.
+# follow the instruction in the Task 5 description and complete the task as per it.
+
+# check if mysql table is created using "patients"
+# Use this final dataset and upload it on the provided database for performing analysis in MySQL
+# To run this task click on the terminal and click on the run project
+
+
 
